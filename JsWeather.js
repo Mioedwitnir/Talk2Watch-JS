fe3562a4ea65ee5520127a5239de51339e07e463
Benjamin Sliwa <benjamin.sliwa@tu-dortmund.de>
2014-11-08 09:33:00 +0100

    Added a first sample for the T2W JS API

---
+++ JsWeather.js
@@ -0,0 +1,44 @@
+Talk2Watch.addEventListener("ready", function(e) {
+    window.navigator.geolocation.getCurrentPosition(locationSuccess,locationError);
+});
+
+
+function locationSuccess(pos) {
+  var coordinates = pos.coords;
+  fetchWeather(coordinates.latitude, coordinates.longitude);
+}
+
+function locationError(err) {
+
+}
+
+function fetchWeather(latitude, longitude) {
+    var response;
+    var req = new XMLHttpRequest();
+    var url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + latitude + "&lon=" + longitude + "&cnt=1";
+
+    req.open('GET', url, true);
+    req.onload = function(e) {
+
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                response = JSON.parse(req.responseText);
+                var temperature;
+                if (response) {
+                    var temp = response.main.temp;
+                    var unit = " C";
+                    if (response.sys.country === "US") {
+                        temperature = Math.round(((temp - 273.15) * 1.8) + 32);
+                        unit = " F";
+                    }
+                    else {
+                        temperature = Math.round(temp - 273.15);
+                    }
+
+                    Talk2Watch.sendSms(temperature + unit, "Weather in " + response.name);
+                }
+            }
+        }
+    };
+    req.send(null);
+}
